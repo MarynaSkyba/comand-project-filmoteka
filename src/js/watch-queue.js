efbaa78efd467ffc7657031426fc03747027b415
Sluchevska <alaska3a@gmail.com>
2021-08-08 22:48:41 +0100

    update

--- src/js/watch-queue.js
+++ src/js/watch-queue.js
@@ -1,13 +1,29 @@
 import Notiflix from 'notiflix';
 import getRefs from './refs';
+import QueryService from './query-service';
 const refs = getRefs();
+// const queryServiceById = new QueryService()
+
+// // 1. empty[]
+// let watchedMovie = [];
+// let queueMovie = [];
+
+// refs.watchedBtnModal.addEventListener('click', addWatchedMovie)
+
+// function addWatchedMovie {
+
+// }
+
+
+// 2. find id movie
+// 3.fetch id on api
 
 
 //1. add to buttons
 
 
 
-// 2.render watchedBTN
+// 2.render watchedBTN(onPage)
 // refs.watchedBtn.addEventListener('click', renderButtonsMarkup)
 // refs.queueBtn.addEventListener('click', renderButtonsMarkup)
 
