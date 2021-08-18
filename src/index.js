70572c8c92cbace63446a46cec28ba59fd6e95af
Maryna <marynaskyba@gmail.com>
2021-08-18 22:05:34 +0300

    add basicLightbox

--- src/index.js
+++ src/index.js
@@ -10,12 +10,4 @@ import './js/chage-btn';
 import './js/trailer';
 
 
-// import templateCard from './template/tmp-card.hbs';
-// import getRefs from './js/refs';
-// import QueryService from './js/query-service.js';
-// import Notiflix from 'notiflix';
-// import changeBtn from './js/chage-btn';
-
-
-
 
